9a242b25b9590b818e3a4f704aab2adb104fe367
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-22 00:05:43 -0700

    line spacing

--- docker.sh
+++ docker.sh
@@ -86,12 +86,14 @@ if [ "$b" = "y" ]; then
     PREV_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
     # Start by removing all previous versions of the image
     if [ ! -z "$PREV_ID" ]; then
-        printf "Deleting old docker image:\n"
+        printf "\nDeleting old docker image:\n"
         docker image rm -f "$PREV_ID"
     fi
     # Build and tag the image with the git commit hash
-    printf "\nBuilding new docker image: $IMG_NAME:$GIT_VER\n"
+    printf "\nBuilding new docker image: latest\n"
     docker build -t $IMG_NAME:latest .
+
+    printf "\nTagging image with git hash: $GIT_VER\n"
     docker tag $IMG_NAME:latest $IMG_NAME:$GIT_VER
 fi
 
@@ -107,11 +109,11 @@ if [ "$versionBump" != "-" ]; then
             NEW_SEM_VER=$(npm version major)
             ;;
         *)
-            printf "version bump must be one of patch, minor, or major\n"
+            printf "\nversion bump must be one of patch, minor, or major\n"
             exit 1
             ;;
     esac
-    printf "Rebuilding with new semantic version: $NEW_SEM_VER\n\n"
+    printf "\nRebuilding with new semantic version: $NEW_SEM_VER\n"
     # Rebuild so that new semantic version is included in the image
     "$0" -b
 fi
@@ -127,7 +129,7 @@ if [ "$p" = "y" ]; then
         | sed -e 's/^[ \t]*//')
 
     # Push the newest commit
-    printf "Pushing new git version to docker repo: $GIT_VER\n"
+    printf "\nPushing new git version to docker repo: $GIT_VER\n"
     docker push $IMG_NAME:$GIT_VER
     # Push the latest tag
     printf "\nPushing latest tag to docker repo: latest\n"
